ae3c76d4c0c5f49c13c3dcb5ddac1beb4fc640fc
SayandeepB9 <sayandeepbhowmick1@gmail.com>
2025-01-15 14:47:44 +0530

    Simple changes

--- exc.py
+++ exc.py
@@ -55,7 +55,11 @@ table = pw.io.gdrive.read(
     mode="streaming",
     with_metadata=True,
 )
+
 pw.io.jsonlines.write(table, "test.jsonl")
+
+embedder = SentenceTransformer("sentence-transformers/all-mini-l6-v2")
+embed_function = lambda text: embedder.encode([text])[0]
 vector_store = VectorStoreServer(
     table,
     parser=pdf_parser,
